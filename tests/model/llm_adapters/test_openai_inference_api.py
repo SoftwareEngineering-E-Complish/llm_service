b389d349ade441567bfc07b8e7de40627fb5f745
nataell95 <nataell.cornu@uzh.ch>
2024-03-15 12:17:39 +0100

    adjust tests

--- tests/model/llm_adapters/test_openai_inference_api.py
+++ tests/model/llm_adapters/test_openai_inference_api.py
@@ -1,19 +1,25 @@
+import json
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 from app.model.llm_adapters.openai_inference_api import OpenAiInference
+
+
+class MockOpenAiInference(OpenAiInference):
+    def __init__(self):
+        pass
 
 class TestOpenAiInference(unittest.TestCase):
 
     def setUp(self):
-        self.openai_inference = OpenAiInference()
+        self.openai_inference = MockOpenAiInference()
         self.mock_chat_compilation_wrong = MagicMock()
-        self.mock_chat_compilation_wrong.chat.completions.create.return_value = 'hiho'
+        self.mock_chat_compilation_wrong.chat.completions.create.return_value = "hiho"
         self.mock_chat_compilation_wrong_arguments = MagicMock()
         self.mock_chat_compilation_wrong_arguments.chat.completions.create.return_value.dict.return_value = {
             'choices': [{'message': {'tool_calls': [{'function': { 'arguments': 'blub' }} ]}}] }
         self.mock_chat_compilation_correct = MagicMock()
         self.mock_chat_compilation_correct.chat.completions.create.return_value.dict.return_value = {
-            'choices': [{'message': {'tool_calls': [{'function': { 'arguments': {2:2} }} ]}}] }
+            'choices': [{'message': {'tool_calls': [{'function': { 'arguments': json.dumps({2:2}) }} ]}}] }
 
     def test_query_llm_invalid_api_documentation_raises(self):
         with self.assertRaises(TypeError):
@@ -28,16 +34,14 @@ class TestOpenAiInference(unittest.TestCase):
 
     def test_query_llm_invalid_response_not_dict_raises(self):
         self.openai_inference._OpenAiInference__client = self.mock_chat_compilation_wrong_arguments
-        with self.assertRaises(TypeError):
+        with self.assertRaises(json.decoder.JSONDecodeError):
             self.openai_inference.query_llm('234',{2:3})
 
 
     def test_query_llm_valid(self):
         self.openai_inference._OpenAiInference__client = self.mock_chat_compilation_correct
         result = self.openai_inference.query_llm('234',{2:3})
-        self.assertEqual(result,{2:2})
- 
-
+        self.assertEqual(json.loads(result),{"2":2})
 
 
 if __name__ == "__main__":
